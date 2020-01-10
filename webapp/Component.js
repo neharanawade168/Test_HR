f643d53b8cdbd8268e6b4075d7142907ac6ef9cd
Neha Ranawade <neharanawade168@gmail.com>
2020-01-10 11:22:13 +0000

    Additional Comment Commit

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,7 +1,7 @@
 jQuery.sap.declare("nw.epm.refapps.st.prod.manage.Component");
 sap.ui.getCore().loadLibrary("sap.ui.generic.app");
 jQuery.sap.require("sap.ui.generic.app.AppComponent");
-
+//this is addition 
 sap.ui.generic.app.AppComponent.extend("nw.epm.refapps.st.prod.manage.Component", {
 	metadata: {
 		"manifest": "json"
